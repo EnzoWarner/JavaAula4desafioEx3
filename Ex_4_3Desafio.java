da5efb0b314c57dffa3fe639874c6042fd57de8c
EnzoWarner <enzo_warner@hotmail.com>
2024-05-21 02:38:58 -0300

    first commit

---
+++ Ex_4_3Desafio.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+public class Ex_4_3Desafio {
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+               int A, B, C, D;
+               System.out.println("Insira o valor de A: ");
+               A = sc.nextInt();
+               System.out.println("Entre com o numero de B: ");
+               B = sc.nextInt();
+               System.out.println("Entre com o numero de C: ");
+               C = sc.nextInt();
+               System.out.println("Entre com o numero de D: ");
+               D = sc.nextInt();
+               int r = a * b - c * d;
+System.out.println(" A diferenca é: " + r);
+}
+}
+}
